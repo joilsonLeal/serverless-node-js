eb34b21e2744bcc7a78b5429f7dad4e3cb63843e
joilson <joilson.bica@somospi.com.br>
2020-10-05 14:50:43 -0300

    chore: inserção de lógica para consulta e inserção no dynamodb

--- index.ts
+++ index.ts
@@ -1,7 +1,62 @@
-class Pessoa {
-    constructor(private _name:string) {}
+import { AWS } from 'aws-sdk';
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-1'});
 
-    get name() {
-        return this._name;
-    }
+exports.handle = function(event: any, context: any, callback: any) {
+
+}
+
+function insertDynamoDB(callback: any) {
+    const params = {
+        Item: {
+            date: Date.now(),
+            message: 'Insert dynamoDB'
+        },
+        
+        TableName: 'lyrics'
+    };
+
+    docClient.put(params, function(err: any, data: any) {
+        if(err) {
+            callback(err, null);
+        }
+        else {
+            callback(null, data)
+        }
+    })
+}
+
+function getDynamoDB(callback: any) {
+    
+    /*
+    const scannigParams = {
+        TableName: 'lyrics',
+        Limit: 100
+    };
+    // scan percorre toda a tabela
+    // utilizar query
+    docClient.scan(scannigParams, function (err: any, data: any) {
+        if(err) {
+            callback(err, null);
+        }
+        else {
+            callback(null, data)
+        }
+    });
+    */
+
+    const params = {
+        TableName: 'lyrics',
+        Key: {
+            "date": 123123123123
+        }
+    };
+
+    docClient.get(params, function (err: any, data: any) {
+        if(err) {
+            callback(err, null);
+        }
+        else {
+            callback(null, data)
+        }
+    });
 }
