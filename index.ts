35b266ed77f967f88874667cae31f81489e366dc
joilson <joilson_rg@hotmail.com>
2020-10-08 10:56:34 -0300

    chore: começo da implementação em typescript

--- index.ts
+++ index.ts
@@ -1,43 +1,53 @@
-import { AWS } from 'aws-sdk';
-const docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-1'});
+import * as AWS from 'aws-sdk';
+global.fetch = require('node-fetch');
 
-export const handler = function(event: any, context: any, callback: any) {
-    
-    const result = getDynamoDB(event.author, event.music, callback);
-    let lyrics = '';
-    if(result == null) {
-        lyrics = getLyrics(event.author, event.music);
-        insertDynamoDB(event.author, event.music, lyrics, callback);
+let options = {};
+if (process.env.IS_OFFLINE) {
+    options = {
+        region: 'localhost',
+        endpoint: 'http://localhost:8000',
+    };
+}
+
+const docClient = new AWS.DynamoDB.DocumentClient(options);
+
+export const handler = async (event: any) => {
+    const { author, music } = event.pathParameters;
+
+    const result = getDynamoDB(author, music);
+    let lyrics: any = '';
+
+    if(!result) {
+        console.log("Inserindo");
+        lyrics = await getLyrics(author, music);
+
+        if(lyrics) 
+            insertDynamoDB(author, music, lyrics);
+        else
+            lyrics = 'Não foi encontrada letra.';
     }
     else {
+        console.log("Tem no banco!");
         lyrics = result;
     }
 
-    return lyrics;
+    return lyrics; 
 }
 
-function insertDynamoDB(author: string, music: string, lyrics: string, callback: any) {
+async function insertDynamoDB(author: string, music: string, lyrics: string) {
     const params = {
         Item: {
-            author: author,
-            music: music,
+            author: author.toLowerCase(),
+            music: music.toLowerCase(),
             lyrics: lyrics
         },
-        
         TableName: 'lyrics'
     };
 
-    docClient.put(params, function(err: any, data: any) {
-        if(err) {
-            return callback(err, null);
-        }
-        else {
-            return callback(null, data)
-        }
-    })
+    const data = await docClient.put(params).promise();
 }
 
-function getDynamoDB(author: string, music: string, callback: any) {
+async function getDynamoDB(author: string, music: string) {
     
     const params = {
         TableName: 'lyrics',
@@ -47,17 +57,17 @@ function getDynamoDB(author: string, music: string, callback: any) {
         }
     };
 
-    docClient.get(params, function (err: any, data: any) {
-        if(err) {
-            callback(err, null);
-        }
-        else {
-            callback(null, data)
-        }
-    });
+    const data = await docClient.get(params).promise();
+    if(data.Item)
+        return data.Item.lyrics;
+    else
+        return '';
 }
 
 async function getLyrics(author: string, music: string): Promise<string> {
-    const response = await fetch(`https://api.lyrics.ovh/v1/${author}/${music}`).then(result => result.json()).catch(err => console.log(err));
-    return response.lyrics;
+    const response = await 
+        fetch(`https://api.lyrics.ovh/v1/${author}/${music}`)
+            .then(result => result.json())
+            .catch(err => console.log(err));
+    return response.Item.lyrics;
 }
