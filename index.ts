a7abd31f70ae6272c33a8dfc1f060ec0adadae94
joilson <joilson_rg@hotmail.com>
2020-10-09 08:48:59 -0300

    chore: utilização do dynamodb

--- index.ts
+++ index.ts
@@ -2,79 +2,68 @@ import * as AWS from 'aws-sdk';
 import{ Context } from 'aws-lambda';
 global.fetch = require('node-fetch');
 
-// let options = {};
-// if (process.env.IS_OFFLINE) {
-//     options = {
-//         region: 'localhost',
-//         endpoint: 'http://localhost:8000',
-//     };
-// }
-
 interface ReqEvent {
     author: string;
     music: string;
 }
 
-// const docClient = new AWS.DynamoDB.DocumentClient(options);
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-2'});
 
 export const handler = async (event: ReqEvent, context: Context) => {
     const { author, music } = event;
 
-    // const result = getDynamoDB(author, music);
+    const result = getDynamoDB(author, music);
     let lyrics: any = '';
 
-    // if(!result) {
-        // console.log("Inserindo");
+    if(!result) {
+        console.log("Inserindo");
         lyrics = await getLyrics(author, music);
 
-    //     if(lyrics) 
-    //         insertDynamoDB(author, music, lyrics);
-    //     else
-    //         lyrics = 'Não foi encontrada letra.';
-    // }
-    // else {
-    //     console.log("Tem no banco!");
-    //     lyrics = result;
-    // }
+        if(lyrics) 
+            insertDynamoDB(author, music, lyrics);
+        else
+            lyrics = 'Não foi encontrada letra.';
+    }
+    else {
+        console.log("Tem no banco!");
+        lyrics = result;
+    }
 
     return lyrics; 
 }
 
-// async function insertDynamoDB(author: string, music: string, lyrics: string) {
-//     const params = {
-//         Item: {
-//             author: author.toLowerCase(),
-//             music: music.toLowerCase(),
-//             lyrics: lyrics
-//         },
-//         TableName: 'lyrics'
-//     };
+async function insertDynamoDB(author: string, music: string, lyrics: string) {
+    const params = {
+        Item: {
+            author: author.toLowerCase(),
+            music: music.toLowerCase(),
+            lyrics: lyrics
+        },
+        TableName: 'lyrics'
+    };
 
-//     const data = await docClient.put(params).promise();
-// }
+    const data = await docClient.put(params).promise();
+}
 
-// async function getDynamoDB(author: string, music: string) {
+async function getDynamoDB(author: string, music: string) {
     
-//     const params = {
-//         TableName: 'lyrics',
-//         Key: {
-//             "author": author,
-//             "music": music
-//         }
-//     };
+    const params = {
+        TableName: 'lyrics',
+        Key: {
+            "author": author,
+            "music": music
+        }
+    };
 
-//     const data = await docClient.get(params).promise();
-//     if(data.Item)
-//         return data.Item.lyrics;
-//     else
-//         return '';
-// }
+    const data = await docClient.get(params).promise();
+    if(data.Item)
+        return data.Item.lyrics;
+    else
+        return '';
+}
 
 async function getLyrics(author: string, music: string): Promise<any> {
     const response = await 
-        // fetch(`https://date.nager.at/api/v2/publicholidays/${author}/${music}`)
-        //     .then(result => result.json())
-        //     .catch(err => console.log(err));
         fetch(`https://api.lyrics.ovh/v1/${author}/${music}`)
             .then(result => result.json())
             .catch(err => {throw Error(err)});
